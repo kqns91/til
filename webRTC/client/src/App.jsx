0f90313695caebd6a500bf4fc9f67d302cc7b942
kqns91 <kansai.119917@gmail.com>
2023-03-11 23:24:55 +0900

    [Fix] ルーム作成後に遷移できてない

--- webRTC/client/src/App.jsx
+++ webRTC/client/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, createBrowserRouter} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import CreateRoom from './components/CreateRoom';
 import Room from './components/Room';
 
 function App() {
+  const history = createBrowserHistory();
+
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <Routes>
-          <Route path="/" element={<CreateRoom />} />
+          <Route path="/" element={<CreateRoom history={history}/>} />
           <Route path="/room/:roomID" element={<Room />} />
         </Routes>
       </BrowserRouter>
--- webRTC/client/src/components/CreateRoom.jsx
+++ webRTC/client/src/components/CreateRoom.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const CreateRoom = (props) => {
+  const { history } = props;
+
   const create = async (e) => {
     e.preventDefault();
 
     const resp = await fetch("http://localhost:8000/create");
     const { room_id } = await resp.json();
-    
-    props.history.push(`/room/${room_id}`)　
+
+    history.push(`/room/${room_id}`)　
   };
 
   return (
