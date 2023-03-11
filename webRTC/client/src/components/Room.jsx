d02ad02e87be3d28b13bb0ebdc6739915d683726
kqns91 <kansai.119917@gmail.com>
2023-03-12 00:06:18 +0900

    [Add] ルームに参加できるようにする

--- webRTC/client/src/components/Room.jsx
+++ webRTC/client/src/components/Room.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import { useParams } from "react-router-dom";
 
 const Room = () => {
+  const roomID = useParams();
+
+  useEffect(() => {
+    const ws = new WebSocket(
+      `ws://localhost:8000/join?roomID=${roomID}`
+    );
+
+    ws.addEventListener("open", () => {
+      ws.send(JSON.stringify({join: "true"}));
+    })
+  })
+
   return (
     <div>
-       
+       <video autoPlay controls={true}></video>
+       <video autoPlay controls={true}></video>
     </div>
   )
 }
